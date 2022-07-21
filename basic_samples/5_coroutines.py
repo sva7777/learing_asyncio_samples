befa645a1c715fe121d2439bee3c048aae685398
vasily <sva7777@gmail.com>
2022-07-21 09:59:00 +0300

    initial commit of samples

---
+++ basic_samples/5_coroutines.py
@@ -0,0 +1,50 @@
+from inspect import getgeneratorstate
+
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+def subgen():
+    x = 'Ready to accept message'
+    message = yield x
+    print ("subget recived:", message)
+
+class BlaBlaException(Exception):
+    pass
+
+
+@coroutine
+def avarage():
+    count  =0
+    sum = 0
+    avarage = None
+
+    while True:
+        try:
+            x = yield avarage
+        except StopIteration:
+            print("Done")
+            break
+        except BlaBlaException:
+            print("bla bla bla")
+            break
+        else:
+            count = count +1
+            sum += x
+            avarage = round(sum/count, 2)
+    return avarage
+
+g = avarage()
+print (getgeneratorstate(g))
+
+print(g.send(4))
+print(g.send(10))
+try:
+    print(g.throw(StopIteration))
+except StopIteration as e:
+    print('Avarage', e.value)
+
---
+++ basic_samples/6_del.py
@@ -0,0 +1,48 @@
+from inspect import getgeneratorstate
+
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+class BlaBlaException(Exception):
+    pass
+
+
+def subgen():
+    while True:
+        try :
+            message = yield
+        except StopIteration :
+            print("Ku-Ku")
+            break
+        else:
+            print("........", message)
+    return "Returned from subgen()"
+
+@coroutine
+def delegator(g):
+    #while True:
+    #    try:
+    #        data = yield
+    #        g.send(data)
+    #    except BlaBlaException as e:
+    #        g.throw(e)
+    result = yield from g
+    print(result)
+
+
+sg = subgen()
+g = delegator(sg)
+
+g.send("vasily")
+g.throw(StopIteration)
+
+#print (next(g))
+#print (next(g))
+#print (next(g))
+#print (next(g))
