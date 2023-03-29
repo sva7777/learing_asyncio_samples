fbb05948ce116e6d54bfec48eb97b33b88e0fbbc
vasily <sva7777@gmail.com>
2023-03-29 10:07:30 +0300

    add converter from List to Binary tree. Usefull for leetcode

---
+++ leetcode_helpers/main.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def ConvertListToBinaryTree(items):
+    n = len(items)
+
+    if n == 0:
+        return None
+
+    def inner(index: int = 0) -> TreeNode:
+        if n <= index or items[index] is None:
+            return None
+
+        node = TreeNode(items[index])
+        node.left = inner(2 * index + 1)
+        node.right = inner(2 * index + 2)
+        return node
+
+    return inner()
